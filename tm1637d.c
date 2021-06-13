00e8e3d4ac72233bea0e6a5b264f3918246f458d
Александр Мишин <mishin@mh.net.ru>
2021-06-13 21:49:09 +0400

    Got data written to device

--- tm1637d.c
+++ tm1637d.c
@@ -28,6 +28,7 @@
 
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <signal.h>
 #include <syslog.h>
 #include <err.h>
@@ -51,11 +52,22 @@
 #define	UNIT_MAX			1
 #define TM1637_CUSE_DEFAULT_DEVNAME	"tm1637"
 
+#define MAX_DIGITS			4
+#define MAX_CHARS			MAX_DIGITS+1
+
+#define CHR_SPACE			0x00
+#define CHR_HYPHEN			0x40
+
 struct pidfh *pfh;
 static char *pid_file = NULL;
+static const uint8_t char_code[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f };
 
 /* tm1637_dev device struct */
 struct tm1637_dev_t {
+    uint8_t	 codes[MAX_DIGITS];
+    char	 digits[MAX_DIGITS];
+    char	 buf_text[MAX_CHARS];
+    size_t	 buf_len;
 };
 
 static int tm1637_cuse_open(struct cuse_dev *, int);
@@ -106,8 +118,22 @@ static int
 tm1637_cuse_write(struct cuse_dev *cdev, int fflags, const void *peer_ptr, int len)
 {
     struct tm1637_dev_t *tmd = cuse_dev_get_priv0(cdev);
+    int error;
 
-    return (0);
+    if ((len == 0) || (len > MAX_CHARS))
+	return (CUSE_ERR_INVALID);
+
+//    TM1637_UNLOCK(ec);
+    error = cuse_copy_in(peer_ptr, tmd->buf_text, len);
+    tmd->buf_len = len;
+//    TM1637_LOCK(ec);
+
+    if (error)
+	return (error);
+
+    printf("%.*s\n", tmd->buf_len, tmd->buf_text);
+
+    return (len);
 }
 
 static void
@@ -150,13 +176,46 @@ daemonize(void)
     pidfile_write(pfh);
 }
 
+static void
+tm1637_create(void)
+{
+    int id;
+    unsigned int n = 0;
+    int unit_num[UNIT_MAX] = { -1 };
+    struct cuse_dev *pdev;
+
+    struct tm1637_dev_t *tmd;
+
+    for (n = 0; n < UNIT_MAX; n++) {
+
+	if ((tmd = malloc(sizeof(*tmd))) != NULL) {
+
+	    if (unit_num[id] < 0) {
+		if (cuse_alloc_unit_number_by_id(&unit_num[id], CUSE_ID_DEFAULT(id)) != 0)
+		    tm1637_errx(1, "Cannot allocate uniq unit number");
+	    }
+
+again:
+	    pdev = cuse_dev_create(&tm1637_cuse_methods, tmd, 0,
+		CDEV_UID, CDEV_GID, CDEV_MODE,
+		"%s.%d", TM1637_CUSE_DEFAULT_DEVNAME, unit_num[id]);
+
+	    /*
+	     * Resolve device naming conflict with new
+	     * kernel evdev module:
+	     */
+	    if (pdev == NULL &&
+		cuse_alloc_unit_number_by_id(&unit_num[id], CUSE_ID_DEFAULT(id)) == 0)
+		    goto again;
+
+	    syslog(LOG_INFO, "Creating /dev/%s.%d\n", TM1637_CUSE_DEFAULT_DEVNAME, unit_num[id]);
+	}
+    }
+}
+
 int
 main(int argc, char **argv)
 {
-    int id;
-    unsigned int n;
-    int unit_num[UNIT_MAX] = { -1 };
-    struct cuse_dev *pdev;
 
     openlog("tm1637d", LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_DAEMON);
 
@@ -164,23 +223,7 @@ main(int argc, char **argv)
 	tm1637_errx(EX_USAGE, "Could not open /dev/cuse. Did you kldload cuse4bsd?");
     }
 
-    n = 0;
-    if (unit_num[id] < 0) {
-	if (cuse_alloc_unit_number_by_id(&unit_num[id], CUSE_ID_DEFAULT(id)) != 0)
-	    tm1637_errx(1, "Cannot allocate uniq unit number");
-    }
-
-again:
-    pdev = cuse_dev_create(&tm1637_cuse_methods, (void *)(long)n,
-	0, CDEV_UID, CDEV_GID, CDEV_MODE, "%s.%d", TM1637_CUSE_DEFAULT_DEVNAME, unit_num[id]);
-
-    /*
-     * Resolve device naming conflict with new
-     * kernel evdev module:
-     */
-    if (pdev == NULL &&
-	cuse_alloc_unit_number_by_id(&unit_num[id], CUSE_ID_DEFAULT(id)) == 0)
-	    goto again;
+    tm1637_create();
 
     for(;;) {
 	tm1637_work(NULL);
